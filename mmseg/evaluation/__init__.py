6d0388322790096a10dd48cb77d6a6284efbd3cc
Muhammad Rifqi Priyo Susanto <muhammadrifqipriyosusanto@gmail.com>
2024-03-21 14:08:00 +0700

    Add DeepGlobe dataset

--- mmseg/evaluation/__init__.py
+++ mmseg/evaluation/__init__.py
@@ -1,4 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .metrics import CityscapesMetric, DepthMetric, IoUMetric
 
-__all__ = ['IoUMetric', 'CityscapesMetric', 'DepthMetric']
+# For ISDNet
+from .metrics_fn import eval_metrics, mean_dice, mean_fscore, mean_iou
+
+__all__ = [
+	'IoUMetric', 'CityscapesMetric', 'DepthMetric',
+	'mean_dice', 'mean_iou', 'mean_fscore', 'eval_metrics'
+]
